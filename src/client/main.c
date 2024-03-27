d7d1d4519b6bd3ef2ae14f188aaba864bad897c1
alexaraujosa <alexaraujosa@hotmail.com>
2024-03-27 11:00:51 +0000

    feat(client_main): adds argument verifier for client executor

--- src/client/main.c
+++ src/client/main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "client/test.h"
 #include "common/test_util.h"
 
 int main(int argc, char const *argv[]) {
     printf("Hello world from client!\n");
     test();
-
     common_test_util();
+
+    if(argc < 2) {
+        printf("Insufficient arguments. Try again later.\n");
+        exit(EXIT_FAILURE);
+    } else {
+        // ...
+    }
+
 
     return 0;
 }
