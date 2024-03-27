804fc3b967bf1abb22bde96a508f43b1f320cb11
alexaraujosa <alexaraujosa@hotmail.com>
2024-03-27 11:11:06 +0000

    feat(server_main): adds argument verifier for server executor and default server files setup
    - Argument Verifier
    [O] Currently the user needs to provide the escalation policy.
    [O] In the future, he will not need to provide a specific policy.
    [O] If not provided, a main policy will be choose by the executor.
    
    - Default Server Files Setup
    [!] Remain clarify the mode of the directory and id file.
    [!] Still needs to implement the output folder provided by the user..
    [!] .. and the actual_history.log file.
    [!] Remain create the history.log file on the persistence data setup folder.

--- src/server/main.c
+++ src/server/main.c
@@ -1,10 +1,45 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include "common/io.h"
 #include "common/test_util.h"
 
 int main(int argc, char const *argv[]) {
     printf("Hello world from server!\n");
-
     common_test_util();
+
+    if(argc < 4) {
+        printf("Insufficient arguments. Try again later.\n");
+        exit(EXIT_FAILURE);
+    } else {
+        // SERVER PERSISTENCE DATA SETUP
+        char* server_data_path = join_paths(3, get_cwd(), "build", "server_data");
+        CREATE_DIR(server_data_path, 0777); //TODO: Verify mode
+
+        // ID SETUP
+        char* id_path = join_paths(2, server_data_path, "id");
+        int fd = open(id_path, O_RDWR | O_CREAT, 0777); //TODO: Verify mode
+        if(fd == -1) { 
+            perror("ERROR! Exception occurred while opening file.\n"); 
+            exit(EXIT_FAILURE); 
+        }
+        
+        int id = 1;
+        ssize_t r = read(fd, &id, sizeof(int));
+        if(r == 0) {
+            ssize_t w = write(fd, &id, sizeof(int));
+            if(w == 0) {
+                perror("ERROR! Exception occurred while writing to file.\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+        
+        
+        close(fd);
+        free(server_data_path);
+        free(id_path);
+    }
 
     return 0;
 }
