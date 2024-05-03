14b873cdc3817653f280ea256c6efb46e7bdf99a
DarkenLM <rafaelsantosfernandes660@gmail.com>
2024-05-03 21:52:07 +0100

    feat(server/worker): added worker process code
    - Added initial code for the worker processes.

---
+++ include/server/worker.h
@@ -0,0 +1,27 @@
+/******************************************************************************
+ *                               WORKER PROCESS                               *
+ *                                                                            *
+ *   The worker process is responsible for executing the different tasks that *
+ * are assigned to it by the operator process. On creation, it shall connect  *
+ * to the event system of the operator process and transmit a ready signal,   *
+ * upon which the operator process shall distribute tasks as they arrive.     *
+ *   The amount of worker processes to be created depends on the server input *
+ * parallel_tasks.                                                            *
+ ******************************************************************************/
+
+#ifndef SERVER_WORKER_H
+#define SERVER_WORKER_H
+
+#include <fcntl.h>
+
+typedef struct worker {
+    pid_t pid;
+    int pipe_write;
+} WORKER, *Worker;
+
+/**
+ * @brief Starts a new worker process.
+ */
+Worker start_worker();
+
+#endif
--- src/server/worker.c
+++ src/server/worker.c
@@ -8,3 +8,85 @@
  *   The amount of worker processes to be created depends on the server input *
  * parallel_tasks.                                                            *
  ******************************************************************************/
+
+#include "server/worker.h"
+#include "server/worker_datagrams.h"
+#include "common/util/alloc.h"
+#include "common/util/string.h"
+#include "common/error.h"
+#include "common/io/io.h"
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <signal.h>
+
+#define LOG_HEADER "[WORKER] "
+#define LOG_HEADER_PID "[WORKER@%d] "
+#define READ 0
+#define WRITE 1
+
+Worker start_worker() {
+    #define ERR NULL
+    ERROR_HEADER
+    int _err_pid = 0;
+
+    // Return value
+    Worker ret = SAFE_ALLOC(Worker, sizeof(WORKER));
+
+    int pfd[2];
+    if (pipe(pfd) != 0) ERROR("Unable to open worker pipe");
+
+    int pid = fork();
+    if (pid == 0) {
+        pid = getpid();
+        _err_pid = pid;
+
+        volatile sig_atomic_t shutdown_requested = 0;
+
+        MAIN_LOG(LOG_HEADER_PID "Ready.\n", pid);
+
+        while (!shutdown_requested) {
+            WORKER_DATAGRAM_HEADER dh = read_worker_datagram_header(pfd[READ]);
+
+            switch (dh.mode) {
+                case WORKER_DATAGRAM_MODE_STATUS: {
+                    WorkerStatusDatagram dg = read_partial_worker_status_datagram(pfd[READ], dh);
+                    MAIN_LOG(LOG_HEADER_PID "Received status request.\n", pid);
+                    break;
+                }
+                case WORKER_DATAGRAM_MODE_EXECUTE: {
+                    WorkerExecuteDatagram dg = read_partial_worker_execute_datagram(pfd[READ], dh);
+                    MAIN_LOG(LOG_HEADER_PID "Received execute request.\n", pid);
+                    break;
+                }
+                case WORKER_DATAGRAM_MODE_SHUTDOWN: {
+                    WorkerShutdownDatagram dg = read_partial_worker_shutdown_datagram(pfd[READ], dh);
+                    MAIN_LOG(LOG_HEADER_PID "Received shutdown request.\n", pid);
+                    shutdown_requested = 1;
+                    break;
+                }
+            }
+        }
+
+        // Shutdown worker
+        MAIN_LOG(LOG_HEADER_PID "Shutting down...\n", pid);
+
+        close(pfd[0]);
+        close(pfd[1]);
+
+        MAIN_LOG(LOG_HEADER_PID "Successfully shutdown.\n", pid);
+        _exit(0);
+    }
+
+    close(pfd[0]);
+    ret->pid = pid;
+    ret->pipe_write = pfd[1];
+    
+    return ret;
+
+    err: {
+        if (_err_pid) MAIN_LOG(LOG_HEADER "[%s:%d] %s", __FILE__, __line, __error);
+        else MAIN_LOG(LOG_HEADER_PID "[%s:%d] %s", _err_pid, __FILE__, __line, __error);
+        return ERR;
+    }
+}
