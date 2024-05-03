5ca19db8ca0d57151045c3d3bb50b982eea6ebdf
DarkenLM <rafaelsantosfernandes660@gmail.com>
2024-05-03 22:09:32 +0100

    test(server/worker): added missing files from 23eae503
    - Added missing header files for the server worker datagram test suite.

---
+++ include/test/server/worker_datagrams.h
@@ -0,0 +1,9 @@
+#ifndef TEST_SERVER_WORKER_DATAGRAMS_H
+#define TEST_SERVER_WORKER_DATAGRAMS_H
+
+/**
+ * @brief Tests the Worker Datagram functions.
+ */
+void test_worker_datagram(char* test_data);
+
+#endif
